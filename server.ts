030aa3d8ec187800acd788ec629b4f0a751e7201
k-yakuniku <game0game7@gmail.com>
2024-04-18 00:52:46 +0900

    commit 3

--- server.ts
+++ server.ts
@@ -13,10 +13,6 @@ const app = express();
 
 // .env
 config();
-// https://zenn.dev/knaka0209/articles/9a72a4790d99e7
-// https://kotsukotsu.work/tech/2020-08-27-vercel-serverless-functions-web-api-%E3%82%92%E3%83%87%E3%83%97%E3%83%AD%E3%82%A4%E3%81%99%E3%82%8B/
-// express API を設置できる server??
-
 const PORT = process.env.PUBLIC_PORT || 5000;
 
 //#region  express expansions
